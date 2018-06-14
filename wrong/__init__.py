e72e5c5d1506b601ee1d3fad56a1b520b03c6f7c
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-06-14 10:28:28 +0900

    Added an importer
     * Importer returns a function given module name and function id
     * Allows the user to get wrong implementation easily (sort = wrong.import('sort', 1))
     * For simpler syntax, a wrong.mini might work better (from wrong.mini import sort)

--- wrong/__init__.py
+++ wrong/__init__.py
@@ -1,1 +1,20 @@
+from .sort import *
+
+
 name = "wrong"
+
+def importer(module_name, function_id):
+    """
+    Imports a function from a module with given name and function id.
+    """
+    if module_name not in globals():
+        raise ValueError('The module {} does not exist.'.format(module_name))
+    
+    module = globals()[module_name]
+    function_name = '{}_{}'.format(module_name, function_id)
+
+    if hasattr(module, function_name):
+        return getattr(module, function_name)
+    else:
+        raise ValueError('The module {} has no function with id {}' \
+            .format(module_name, function_id))
