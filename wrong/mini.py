14dd7526f23492bc7aa4efecfd77c5d14e4de8c9
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-06-14 10:33:55 +0900

    Added wrong.mini module
     * Allows for a simpler syntax than the interpreter (from wrong.mini import sort)
     * Most informative wrong implementations of each module should be included in the wrong.mini module

---
+++ wrong/mini.py
@@ -0,0 +1,7 @@
+"""
+A mini version of the wrong package. Contains one wrong implementation for
+each module. Allows for a simpler syntax (from wrong.mini import sort). The
+wrong implementation that is most informative to the user should be selected.
+"""
+
+from .sort import sort_1 as sort
