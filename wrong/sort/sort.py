64e5fbc9af8dbd54b86d379d0510d23307802551
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-06-14 10:26:31 +0900

    Added stub sort module to use as template
     * Functions have separate folders
     * The file with same name as the function should only have different incorrect implementations
     * The folder can contain other files with helper functions

---
+++ wrong/sort/sort.py
@@ -0,0 +1,24 @@
+"""
+A collection of implementations of the sort() function.
+"""
+
+
+def sort_0(l):
+    """
+    A correct implementation of the sort() function.
+    """
+    l.sort()
+
+def sort_1(l):
+    """
+    An incorrect implementation of the sort() function. Does not change the
+    given list in any way.
+    """
+    pass
+
+def sort_2(l):
+    """
+    An incorrect implementation of the sort() function. Reverses the given
+    list.
+    """
+    return list(reversed(l))
